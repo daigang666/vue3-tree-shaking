e19561b83ef1904f02585c4badd7bed266c5475a
DaiGang <42136433+daigang666@users.noreply.github.com>
2024-08-09 11:42:25 +0800

    修复编译异常

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,7 @@
 import { parse } from "@vue/compiler-sfc";
 import { transform } from "@vue/compiler-core";
 import type { RootNode } from "@vue/compiler-core";
-import * as Babel from "@babel/standalone";
-
+import { transform as BabelTransform } from "@babel/standalone";
 import { createVariableCollector } from "./variable_collector.js";
 import babelPluginShakeVueScript from "./babelPluginShakeVueScript.js";
 
@@ -24,7 +23,7 @@ function getTemplateUsedVars(templateAst?: RootNode) {
 }
 // 移除script中未使用的变量
 function removeUnusedVars(scriptCode: string, usedVariables: Set<string>) {
-  const result = Babel.transform(scriptCode, {
+  const result = BabelTransform(scriptCode, {
     plugins: [[babelPluginShakeVueScript, { referencedIdentifiers: usedVariables }]],
   });
   return result;
@@ -37,30 +36,33 @@ function removeUnusedVars(scriptCode: string, usedVariables: Set<string>) {
  */
 export function treeShakeVueSFC(sfcContent: string) {
   const { descriptor } = parse(sfcContent);
-  if (!descriptor.template || !descriptor.template.ast) {
+  const { template, scriptSetup, styles } = descriptor;
+  if (!template || !template.ast) {
     console.warn("SFC does not contain a template.");
   }
-  if (!descriptor.scriptSetup) {
+  if (!scriptSetup) {
     console.warn("SFC does not contain a script setup.");
     return { code: sfcContent };
   }
-  const templateAst = descriptor.template?.ast;
+  const templateAst = template?.ast;
 
   const usedVariables = getTemplateUsedVars(templateAst);
-  const babelResult = removeUnusedVars(descriptor.scriptSetup.content, usedVariables);
+  const babelResult = removeUnusedVars(scriptSetup.content, usedVariables);
 
-  const code = `<template>${descriptor.template?.content ?? ""}</template>\n
-    <script>\n${babelResult.code}\n</script>\n
-    ${
-      descriptor.styles
-        ?.map(
-          (style) =>
-            `<style ${style.lang ? `lang="${style.lang}"` : ""}${style.scoped ? " scoped" : ""}>\n${
-              style.content
-            }\n</style>\n`
-        )
-        ?.join("\n") ?? ""
-    }`;
+  const code = `<template>${template?.content ?? ""}</template>
+<script${scriptSetup.setup ? " setup" : ""}${scriptSetup.lang ? ` lang="${scriptSetup.lang}"` : ""}>\n${
+    babelResult.code
+  }\n</script>
+${
+  styles
+    ?.map(
+      (style) =>
+        `<style ${style.lang ? `lang="${style.lang}"` : ""}${style.scoped ? " scoped" : ""}>\n${
+          style.content
+        }\n</style>\n`
+    )
+    ?.join("\n") ?? ""
+}`;
 
   // console.log("vue代码tree shake后的结果:\n", code);
   return { code, parse, descriptor, babelResult, usedVariables };
